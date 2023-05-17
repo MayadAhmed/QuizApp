5ba48af17b1b84a7dce15bccacc47b6499396078
MayadAhmed <128182607+MayadAhmed@users.noreply.github.com>
2023-05-17 16:25:46 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "https://github.com/MayadAhmed/yummy-food-App", function () {
+   console.log("callback - particles.js config loaded");
+   
+});
